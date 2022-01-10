5be9efa62694dcfb6afc875ae27b96630bdaa995
Alabhya Jindal <alabhya10@gmail.com>
2022-01-10 22:47:08 +0530

    smooth scrolling

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const button = document.querySelector(".nav-toggle");
 const nav = document.querySelector(".header");
 
+const navLinks = document.querySelectorAll(".nav-link");
+
 button.addEventListener("click", function () {
   nav.classList.toggle("nav-open");
+});
+
+navLinks.addEventListener("click", function () {
+  nav.classList.remove(".nav-open");
 });
