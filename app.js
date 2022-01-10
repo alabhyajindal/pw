d52488b7059aa63a2a9fe4c50202dbfd2795d7e5
Alabhya Jindal <alabhya10@gmail.com>
2022-01-10 23:37:07 +0530

    glassmorphism

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const button = document.querySelector(".nav-toggle");
 const nav = document.querySelector(".header");
+let currentScroll = 0;
 
 const navLinks = document.querySelectorAll(".nav-link");
 
@@ -11,4 +12,14 @@ navLinks.forEach((item) => {
   item.addEventListener("click", (event) => {
     nav.classList.toggle("nav-open");
   });
+});
+
+window.addEventListener("scroll", function () {
+  currentScroll = document.documentElement.scrollTop;
+
+  if (currentScroll > 160) {
+    nav.classList.add("glassmorphic-nav");
+  } else {
+    nav.classList.remove("glassmorphic-nav");
+  }
 });
